f3f1d8f75b5d57d071fabaac1f65b7276b8d82f6
SUNIL KUMAR <50536429+TharVid@users.noreply.github.com>
2020-04-19 10:02:05 +0530

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,9 @@
+function loadUrls(){
+	//fetch urls
+}
+function saveUrls(){
+	//Fethch urls
+}
+
+document.getElementById('button').addEventListener('clcik',loadUrls);
+document.getElementById('button').addEventListener('clcik',saveUrls);
